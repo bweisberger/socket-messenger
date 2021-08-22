e044500cf6caf8408e9c0571282743926522dfe1
Bill Weisberger <bill.weisberger@gmail.com>
2021-08-21 20:33:14 -0600

    update index.js to ES6, add MessageService call

--- express-generator-project/src/routes/index.js
+++ express-generator-project/src/routes/index.js
@@ -1,8 +1,21 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+const router = express.Router();
+import Message from '../domain/Message';
+import { getErrorStatus } from '../utils/errorHandlers';
 
 router.get('/', (req, res) => {
   res.status(200).json({message: 'Welcome to your server'});
 });
 
-module.exports = router;
+router.post('/message', (req, res) => {
+  console.log('received request to save message: ', req.body);
+  try {
+    const newMessage = new Message(req.body);
+    MessageService.submit(req.body);
+  } catch(err) {
+    res.status(getErrorStatus(err)).send(err.message);
+    console.log(err);
+  }
+})
+
+export default router;
