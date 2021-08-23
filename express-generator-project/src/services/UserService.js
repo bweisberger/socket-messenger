40f4485a36d270484b47032ab08ffb8d5050b290
Bill Weisberger <bill.weisberger@gmail.com>
2021-08-23 15:12:01 -0600

    Add createNewUser to UserService

--- express-generator-project/src/services/UserService.js
+++ express-generator-project/src/services/UserService.js
@@ -1,14 +1,23 @@
 import UserAccess from '../access/UserAccess';
+import User from '../domain/User';
 
 const UserService = {
-  getExistingUsers(toUser, fromUser) {
-    const result = UserAccess.getUsers(toUser, fromUser);
+  getExistingUsers: async (toUser, fromUser) => {
+    const result = await UserAccess.getExistingUserRecords(toUser, fromUser);
+    if (!result.rows) {
+      throw new Error(`No users exist with names: ${toUser}, ${fromUser}`)
+    }
     return result.rows.map(row => {
       return {
         id: row.id,
         name: row.name
       }
     })
+  },
+
+  createNewUser: async (json) => {
+    const newUser = User.fromJsonInput(json);
+    UserAccess.insertNewUser(newUser);
   }
 }
 
