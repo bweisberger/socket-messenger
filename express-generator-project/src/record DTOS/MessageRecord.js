f2febce1161546b0d0dbd603d86801604db3474a
Bill Weisberger <bill.weisberger@gmail.com>
2021-08-23 15:11:44 -0600

    Add former metadata fields to Record

--- express-generator-project/src/record DTOS/MessageRecord.js
+++ express-generator-project/src/record DTOS/MessageRecord.js
@@ -1,10 +1,18 @@
 class MessageRecord {
-  constructor(messageText) {
-    this.text = messageText;
+  constructor(text, toUserName, fromUserName, timeSent, timeRead) {
+    this.text = text;
+    this.toUserName = toUserName;
+    this.fromUserName = fromUserName;
+    this.timeSent = timeSent;
+    this.timeRead = timeRead;
   }
 
-  static from({ message }) {
-    return new MessageRecord(message);
+  static fromNewMessage({ text, toUser, fromUser, timeSent }) {
+    return new MessageRecord(text, toUser, fromUser, timeSent, null);
+  }
+
+  static fromDatabase({ text, toUser, fromUser, timeSent, timeRead }) {
+    return new MessageRecord(text, toUser, fromUser, timeSent, timeRead);
   }
 }
 
